988f852e4964d3301f19d208f91d591cc4c341d1
threen4846 <threen4846@naver.com>
2023-01-06 16:02:43 +0900

    Add files via upload

--- js/scrollanimation.js
+++ js/scrollanimation.js
@@ -18,36 +18,36 @@ $(function () {
             $('#header').show();
         };
 
-        // if (sct > 700 && scene01 == true) {
-        //     scene01 = false;
-        //     $('#leftbar').stop()
-        // } else if (sct < 500 && scene01 == false) {
-        //     scene01 = true;
+        if (sct > 700 && scene01 == true) {
+            scene01 = false;
+            $('#leftbar').stop()
+        } else if (sct < 500 && scene01 == false) {
+            scene01 = true;
 
-        // };
+        };
 
-        // if (sct > 1700 && scene02 == true) {
-        //     scene02 = false;
-        //     $('#percent .percent01 .percentbox>div').stop().delay(0).animate({ left: 0 }, 1500)
-        //     $('#percent .percent02 .percentbox>div').stop().delay(500).animate({ left: 0 }, 1500)
-        //     $('#percent .percent03 .percentbox>div').stop().delay(1000).animate({ left: 0 }, 1500)
-        //     $('#percent .percent04 .percentbox>div').stop().delay(1500).animate({ left: 0 }, 1500)
-        //     $('#percent .percent05 .percentbox>div').stop().delay(2000).animate({ left: 0 }, 1500)
-        // } else if (sct < 1500 && scene02 == false) {
-        //     scene02 = true;
-        //     $('#percent .percent01 .percentbox>div').stop().animate({ left: '-280px' })
-        //     $('#percent .percent02 .percentbox>div').stop().animate({ left: '-50px' })
-        //     $('#percent .percent03 .percentbox>div').stop().animate({ left: '-200px' })
-        //     $('#percent .percent04 .percentbox>div').stop().animate({ left: '-0px' })
-        //     $('#percent .percent05 .percentbox>div').stop().animate({ left: '-50px' })
-        // };
+        if (sct > 1700 && scene02 == true) {
+            scene02 = false;
+            $('#percent .percent01 .percentbox>div').stop().delay(0).animate({ left: 0 }, 1500)
+            $('#percent .percent02 .percentbox>div').stop().delay(500).animate({ left: 0 }, 1500)
+            $('#percent .percent03 .percentbox>div').stop().delay(1000).animate({ left: 0 }, 1500)
+            $('#percent .percent04 .percentbox>div').stop().delay(1500).animate({ left: 0 }, 1500)
+            $('#percent .percent05 .percentbox>div').stop().delay(2000).animate({ left: 0 }, 1500)
+        } else if (sct < 1500 && scene02 == false) {
+            scene02 = true;
+            $('#percent .percent01 .percentbox>div').stop().animate({ left: '-280px' })
+            $('#percent .percent02 .percentbox>div').stop().animate({ left: '-150px' })
+            $('#percent .percent03 .percentbox>div').stop().animate({ left: '-200px' })
+            $('#percent .percent04 .percentbox>div').stop().animate({ left: '-50px' })
+            $('#percent .percent05 .percentbox>div').stop().animate({ left: '-50px' })
+        };
 
-        // if (sct > 2700 && scene03 == true) {
-        //     scene03 = false;
+        if (sct > 2700 && scene03 == true) {
+            scene03 = false;
 
-        // } else if (sct < 2500 && scene03 == false) {
-        //     scene03 = true;
+        } else if (sct < 2500 && scene03 == false) {
+            scene03 = true;
 
-        // };
+        };
     });
 });
